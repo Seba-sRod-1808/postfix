bf4fbdc0dac38755784601d7bcee457c06c59f2e
Cris <crisjavi1307@gmail.com>
2026-02-03 08:46:55 -0600

    Fix en la clase ArrayStack

--- src/ArrayStack.java
+++ src/ArrayStack.java
@@ -2,35 +2,35 @@ package src;
 import java.util.ArrayList;
 
 public class ArrayStack<T> implements Stack<T> {
-    private ArrayList<T> elements;
+    private ArrayList<T> items;
 
     public ArrayStack() {
-        elements = new ArrayList<>();
+        items = new ArrayList<>();
     }
 
     @Override
     public void push(T element) {
-        elements.add(element);
+        items.add(element);
     }
 
     @Override
     public T pop() {
-        if(elements.isEmpty()) {
+        if(items.isEmpty()) {
             throw new RuntimeException("El stack está vacío");
         }
-        return elements.remove(elements.size() - 1);
+        return items.remove(items.size() - 1);
     }
 
     @Override
     public T peek() {
-        if (elements.isEmpty()) {
+        if (items.isEmpty()) {
             throw new RuntimeException("El stack está vacío");
         }
-        return elements.get(elements.size() - 1);
+        return items.get(items.size() - 1);
     }
 
     public boolean isEmpty() {
-        return elements.isEmpty();
+        return items.isEmpty();
     }
 }
 
